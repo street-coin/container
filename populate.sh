54c955f832bba18fc2bd296b4e4e9e3e71370887
luctst <lucas.tostee@gmail.com>
2021-03-21 20:20:07 +0100

    Create populate script add data in local mongo

---
+++ populate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+getBackup() {
+
+}
+
+echo "Populating street-coin database in local.."
+docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/dump-streetcoin.gz" --gzip --db streetcoin --uri "$DB_URL" && mongorestore --gzip --nsInclude="streetcoin.*" --archive="./tmp/dump-streetcoin.gz"'
