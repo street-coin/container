ce486dd7061ba7286eb3e12b29d56ec27300fb09
luctst <lucas.tostee@gmail.com>
2021-03-22 19:39:28 +0100

    Fix syntax error

--- populate.sh
+++ populate.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-getBackup() {
-
-}
-
 echo "Populating street-coin database in local.."
 docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/dump-streetcoin.gz" --gzip --db streetcoin --uri "$DB_URL" && mongorestore --gzip --nsInclude="streetcoin.*" --archive="./tmp/dump-streetcoin.gz"'
